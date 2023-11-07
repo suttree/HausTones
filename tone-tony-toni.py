3d3eb6915ef66af8aea407c75a36bc4ff4f17ede
Duncan Gough <duncan@suttree.com>
2023-11-07 18:46:45 +0000

    bass note / 12

--- tone-tony-toni.py
+++ tone-tony-toni.py
@@ -4,9 +4,11 @@ import time
 
 # Parameters
 scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]  # C major scale frequencies (C4 to C5)
+bass_scale = [130.81, 146.83, 164.81]  # Some bass frequencies (C3, D3, E3)
 fs = 44100  # Sampling rate in Hertz
-max_interval = 0.5  # Maximum interval in seconds between notes
+max_interval = 0.2  # Maximum interval in seconds between notes
 phase_increment = 0.01  # How quickly the "wind" changes
+tone_counter = 0  # Count the number of tones played
 
 # Global variables for keeping track of state
 current_phase = 0.0  # Phase for the sine-wave-based interval timing
@@ -19,20 +21,33 @@ def generate_tone(frequency, sample_count):
     return tone
 
 # Callback function for the stream
-def callback(outdata, frames, time, status):
-    global current_phase, next_frequency
+def callback(outdata, frames, time_info, status):
+    global current_phase, next_frequency, tone_counter
     if status:
         print(status)
-    current_time = time.outputBufferDacTime
     interval = (np.sin(current_phase) + 1) / 2 * max_interval
     # Fill the output buffer with the current frequency tone
-    outdata[:] = generate_tone(next_frequency, frames).reshape(-1, 1)
+    tone_data = generate_tone(next_frequency, frames)
+    if tone_counter % 12 == 0:  # Play bass note on every 12th tone
+        bass_frequency = np.random.choice(bass_scale)
+        bass_data = generate_tone(bass_frequency, frames)
+        tone_data += bass_data  # Add bass tone to the normal tone for overlap
+    outdata[:] = tone_data.reshape(-1, 1)
     # Update phase and frequency at intervals based on the sine wave
+    current_time = time_info.outputBufferDacTime
     if (current_time % interval) < (frames / fs):
         current_phase += phase_increment
         if current_phase >= (2 * np.pi):
-            current_phase -= (2 * np.pi)
+            current_phase = 0
         next_frequency = np.random.choice(scale)
+        tone_counter += 1
+
+# Normalize and prevent clipping in the callback
+def safe_normalize(samples):
+    peak = np.max(np.abs(samples))
+    if peak > 0:
+        return samples / peak
+    return samples
 
 # Open an output stream
 stream = sd.OutputStream(
