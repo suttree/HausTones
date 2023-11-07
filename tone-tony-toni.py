dec4a220797184f41e68a841919b3514949624b6
Duncan Gough <duncan@suttree.com>
2023-11-07 18:53:10 +0000

    tone loss

--- tone-tony-toni.py
+++ tone-tony-toni.py
@@ -4,9 +4,8 @@ import time
 
 # Parameters
 scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]  # C major scale frequencies (C4 to C5)
-bass_scale = [130.81, 146.83, 164.81]  # Some bass frequencies (C3, D3, E3)
 fs = 44100  # Sampling rate in Hertz
-max_interval = 0.2  # Maximum interval in seconds between notes
+max_interval = 0.5  # Maximum interval in seconds between notes
 phase_increment = 0.01  # How quickly the "wind" changes
 tone_counter = 0  # Count the number of tones played
 
@@ -17,8 +16,10 @@ next_frequency = np.random.choice(scale)  # Next note to play
 # Function to generate tones with a specific frequency
 def generate_tone(frequency, sample_count):
     t = (np.arange(sample_count) / fs)
-    tone = np.sin(2 * np.pi * frequency * t)
+    #tone = np.sin(2 * np.pi * frequency * t)
+    tone = np.pi * frequency * t
     return tone
+    
 
 # Callback function for the stream
 def callback(outdata, frames, time_info, status):
@@ -28,11 +29,13 @@ def callback(outdata, frames, time_info, status):
     interval = (np.sin(current_phase) + 1) / 2 * max_interval
     # Fill the output buffer with the current frequency tone
     tone_data = generate_tone(next_frequency, frames)
-    if tone_counter % 12 == 0:  # Play bass note on every 12th tone
-        bass_frequency = np.random.choice(bass_scale)
-        bass_data = generate_tone(bass_frequency, frames)
-        tone_data += bass_data  # Add bass tone to the normal tone for overlap
-    outdata[:] = tone_data.reshape(-1, 1)
+    if tone_counter % 12 == 0:  # Add an octave or harmony on every 12th tone
+        # Choose whether to go an octave above or below
+        octave_factor = 2 if (tone_counter // 12) % 2 == 0 else 0.5
+        octave_frequency = next_frequency * octave_factor
+        octave_data = generate_tone(octave_frequency, frames)
+        tone_data += octave_data  # Add octave tone to the normal tone for overlap
+    outdata[:] = safe_normalize(tone_data).reshape(-1, 1)
     # Update phase and frequency at intervals based on the sine wave
     current_time = time_info.outputBufferDacTime
     if (current_time % interval) < (frames / fs):
