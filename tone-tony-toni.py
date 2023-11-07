b569d42f24a1875fa68300b34ed7813883416c41
Duncan Gough <duncan@suttree.com>
2023-11-07 18:57:18 +0000

    this is the shit

--- tone-tony-toni.py
+++ tone-tony-toni.py
@@ -1,16 +1,21 @@
 import numpy as np
 import sounddevice as sd
 import itertools
-import time
+import threading
 
 # Parameters
 scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]  # C major scale frequencies (C4 to C5)
 fs = 44100  # Sampling rate in Hertz
-note_duration = 0.5  # Duration of each note in seconds
+fast_note_duration = 0.5  # Duration of each fast note in seconds
+slow_note_duration = 0.75  # Duration of each slow note in seconds
 
 # Generate a sequence that goes forwards and backwards through the scale
 full_scale = scale + scale[-2:0:-1]  # Forward and backward without repeating ends
-tone_sequence = itertools.cycle(full_scale)  # Create an iterator that cycles through the sequence
+fast_sequence = itertools.cycle(full_scale)  # Fast sequence iterator
+slow_sequence = itertools.cycle(full_scale)  # Slow sequence iterator
+
+# Lock for thread safety when accessing the slow sequence
+sequence_lock = threading.Lock()
 
 # Function to generate tones with a specific frequency
 def generate_tone(frequency, duration, fs):
@@ -18,27 +23,53 @@ def generate_tone(frequency, duration, fs):
     tone = np.sin(2 * np.pi * frequency * t)
     return tone
 
+# State for managing current tone and time
+current_fast_tone = next(fast_sequence)
+current_slow_tone = next(slow_sequence)
+fast_elapsed = 0
+slow_elapsed = 0
+
 # Callback function for the stream
 def callback(outdata, frames, time_info, status):
-    global tone_sequence
+    global current_fast_tone, current_slow_tone, fast_elapsed, slow_elapsed
+
     if status:
         print(status)
-    tone_data = generate_tone(next(tone_sequence), note_duration, fs)
-    outdata[:] = tone_data[:frames].reshape(-1, 1)
-    if frames < len(tone_data):
-        # If the buffer is not filled, append zeros
-        outdata[frames:] = 0
+
+    # Generate tones for the current frame
+    fast_tone_data = generate_tone(current_fast_tone, fast_note_duration, fs)[:frames]
+    with sequence_lock:  # Ensure thread-safe access to slow_sequence
+        slow_tone_data = generate_tone(current_slow_tone, slow_note_duration, fs)[:frames]
+
+    # Mix the fast and slow tones
+    mixed_data = fast_tone_data + slow_tone_data
+    mixed_data = mixed_data * (0.5 / mixed_data.max())  # Normalize to prevent clipping
+
+    outdata[:] = mixed_data.reshape(-1, 1)
+
+    # Update the elapsed time for each sequence and get new tones if needed
+    fast_elapsed += frames
+    slow_elapsed += frames
+
+    if fast_elapsed >= int(fast_note_duration * fs):
+        fast_elapsed = 0  # Reset elapsed time for the fast sequence
+        current_fast_tone = next(fast_sequence)  # Get the next tone in the fast sequence
+
+    if slow_elapsed >= int(slow_note_duration * fs):
+        slow_elapsed = 0  # Reset elapsed time for the slow sequence
+        with sequence_lock:  # Ensure thread-safe access to slow_sequence
+            current_slow_tone = next(slow_sequence)  # Get the next tone in the slow sequence
 
 # Open an output stream
 stream = sd.OutputStream(
     channels=1,
     samplerate=fs,
-    blocksize=int(note_duration * fs),  # Set the block size to the note duration
+    blocksize=int(min(fast_note_duration, slow_note_duration) * fs),  # Block size set to the shorter duration note
     callback=callback
 )
 
 # Start playing the sequence
-print("Starting sequence... Press Ctrl+C to stop.")
+print("Starting sequences... Press Ctrl+C to stop.")
 
 try:
     with stream:
@@ -47,4 +78,4 @@ except KeyboardInterrupt:
     pass
 finally:
     stream.close()
-    print("Sequence stopped.")
+    print("Sequences stopped.")
