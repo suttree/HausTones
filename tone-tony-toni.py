671507ec0e00918e06efac023f4a6bb218ef43ef
Duncan Gough <duncan@suttree.com>
2023-11-07 18:33:48 +0000

    threading attempts

--- tone-tony-toni.py
+++ tone-tony-toni.py
@@ -1,53 +1,69 @@
 import numpy as np
 import sounddevice as sd
 import time
+import queue
+import threading
 
 # Parameters
 scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]  # C major scale frequencies (C4 to C5)
 fs = 44100  # Sampling rate in Hertz
 base_duration = 1.0  # Base duration in seconds for a tone
-max_interval = 1.5  # Max interval in seconds between successive tones
-attack_duration = 0.1  # Duration of the attack (fade-in) in seconds
-release_duration = 0.3  # Duration of the release (fade-out) in seconds
+max_interval = 0.5  # Shorter max interval in seconds between successive tones
+attack_duration = 0.01  # Shorter duration of the attack (fade-in) in seconds
+release_duration = 0.01  # Shorter duration of the release (fade-out) in seconds
+tone_queue = queue.Queue()  # Queue to manage tones
 
 # Generate a tone with a specific frequency and duration
 def generate_tone(frequency, duration, fs, attack_duration, release_duration):
-    # Generate time array
     t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    # Create the envelope
     envelope = np.ones_like(t)
     attack_samples = int(attack_duration * fs)
     release_samples = int(release_duration * fs)
-    envelope[:attack_samples] = np.linspace(0, 1, attack_samples)  # Attack
-    envelope[-release_samples:] = np.linspace(1, 0, release_samples)  # Release
-    # Generate the tone with the envelope applied
+    envelope[:attack_samples] = np.linspace(0, 1, attack_samples)
+    envelope[-release_samples:] = np.linspace(1, 0, release_samples)
     tone = np.sin(2 * np.pi * frequency * t) * envelope
     return tone
 
-# Function to play a frequency
-def play_tone(frequency, duration):
-    tone = generate_tone(frequency, duration, fs, attack_duration, release_duration)
-    tone *= 32767 / np.max(np.abs(tone))  # Normalize the tone's volume
-    tone = tone.astype(np.int16)  # Convert to 16-bit format
-    sd.play(tone, samplerate=fs)
-    sd.wait()  # Wait for the tone to finish
+# Function to play tones from the queue
+def play_from_queue():
+    while True:
+        item = tone_queue.get()  # Block until a tone is available
+        if item is None:
+            break  # None is the signal to stop
+        sd.play(item, samplerate=fs, blocking=False)
+        tone_queue.task_done()
 
-def wind_chime():
-    last_play_time = time.time() - max_interval  # Initialize with an offset to play immediately
+# Start the tone player thread
+player_thread = threading.Thread(target=play_from_queue)
+player_thread.start()
+
+# Function to add a frequency to the queue
+def queue_tone(frequency, duration):
+    tone = generate_tone(frequency, duration, fs, attack_duration, release_duration)
+    tone *= 32767 / np.max(np.abs(tone))
+    tone = tone.astype(np.int16)
+    tone_queue.put(tone)
+
+# Start generating and queuing tones
+print("Starting wind chime... Press Ctrl+C to stop.")
+try:
+    next_play_time = time.time() - max_interval  # Start immediately
     while True:
         current_time = time.time()
-        # Check if it's time to play a new tone based on a sine wave interval
-        if current_time - last_play_time > (np.sin(current_time) + 1) / 2 * max_interval:
+        if current_time >= next_play_time:
             # Randomly select a frequency from the scale
             frequency = np.random.choice(scale)
             # Randomize duration to vary the sound
-            duration = np.random.uniform(0.3, base_duration)
-            play_tone(frequency, duration)
-            last_play_time = current_time
-
-# Run the wind chime in a try-except block to catch KeyboardInterrupt
-try:
-    print("Starting wind chime... Press Ctrl+C to stop.")
-    wind_chime()
+            duration = np.random.uniform(0.5, base_duration)
+            queue_tone(frequency, duration)
+            # Schedule the next tone based on a sine wave interval
+            next_play_time += (np.sin(current_time * 0.5) + 1) / 2 * max_interval
+        time.sleep(0.01)
 except KeyboardInterrupt:
-    print("Wind chime stopped by user.")
+    print("Stopping wind chime...")
+finally:
+    # Stop the player thread
+    tone_queue.put(None)  # Signal to stop the thread
+    player_thread.join()
+    sd.stop()  # Stop any remaining sound
+    print("Wind chime stopped.")
