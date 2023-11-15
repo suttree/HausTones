c92a50fa0e39a933151e4883f5af96585ca7a0f2
Duncan Gough <duncan@suttree.com>
2023-11-15 14:42:27 +0000

    chimee

--- scales4ever.py
+++ scales4ever.py
@@ -7,10 +7,15 @@ import pprint, random
 pp = pprint.PrettyPrinter(indent=4)
 
 # Define key and scale
-key = Note('C')
-scale = Scale(key, 'major')
+#key = Note('C')
+#scale = Scale(key, 'major')
+
+key = Note(random.choice(Note.NOTES))
+scales = ['major', 'minor', 'melodicminor', 'harmonicminor', 'pentatonicmajor', 'bluesmajor', 'pentatonicminor', 'bluesminor', 'augmented', 'diminished', 'chromatic', 'wholehalf', 'halfwhole', 'wholetone', 'augmentedfifth', 'japanese', 'oriental', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian']
+
+scale = Scale(key, random.choice(scales))
 progression = Chord.progression(scale, base_octave=key.octave)
-chord = progression[0]
+chord = progression[0].invert_down()
 
 time = 0.0 # Keep track of currect note placement time in seconds
 
@@ -26,21 +31,32 @@ pp.pprint(notes)
 
 offset = 0.6
 interval = 0.2
-for i in range(8):
-    timeline.add(time + interval * 1, Hit(chord.notes[2].transpose(12), 4.0))
-    timeline.add(time + interval * 2, Hit(chord.notes[1].transpose(12), 4.0))
-    timeline.add(time + interval * 3, Hit(chord.notes[0].transpose(12), 4.0))
+for i in range(80):
+    timeline.add(time + interval * 1, Hit(chord.notes[2].transpose(12), 8.0))
+    timeline.add(time + interval * 2, Hit(chord.notes[1].transpose(12), 8.0))
+    timeline.add(time + interval * 3, Hit(chord.notes[0].transpose(12), 8.0))
     timeline.add(time + interval * 4, Hit(chord.notes[2], 4.0))
     timeline.add(time + interval * 5, Hit(chord.notes[1], 4.0))
     timeline.add(time + interval * 6, Hit(chord.notes[0], 4.0))
 
     o_interval = interval + offset
-    timeline.add(time + o_interval * 1, Hit(chord.notes[0].transpose(0), 4.0))
-    timeline.add(time + o_interval * 2, Hit(chord.notes[1].transpose(0), 4.0))
-    timeline.add(time + o_interval * 3, Hit(chord.notes[2].transpose(0), 4.0))
+    timeline.add(time + o_interval * 1, Hit(chord.notes[0].transpose(0), 8.0))
+    timeline.add(time + o_interval * 2, Hit(chord.notes[1].transpose(0), 8.0))
+    timeline.add(time + o_interval * 3, Hit(chord.notes[2].transpose(0), 8.0))
 
     time += 2.2
-    interval += 0.03147
+    interval += 0.025
+
+    if(i % 4 == 0 and i > 0):
+        scale = Scale(key, random.choice(scales))
+        progression = Chord.progression(scale, base_octave=key.octave)
+        chord = progression[0]
+
+    if(i % 20 == 0 and i > 0):
+        timeline.add(time + 0.03, Hit(chord.notes[0].transpose(-12), 4.0))
+        timeline.add(time + 0.04, Hit(chord.notes[1].transpose(-24), 4.0))
+        timeline.add(time + 0.05, Hit(chord.notes[2].transpose(-36), 4.0))
+        time += 2.0
 
 print("Rendering audio...")
 data = timeline.render()
