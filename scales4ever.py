5d90f457d7b2d4082b1aca3a68be708c77b07af4
Duncan Gough <duncan@suttree.com>
2023-11-16 18:32:42 +0000

    wip bleeps

--- scales4ever.py
+++ scales4ever.py
@@ -11,7 +11,8 @@ pp = pprint.PrettyPrinter(indent=4)
 # add in more notes?
     # for j, _note in enumerate(chord):
 # sin/cos on the duration and offset wobbles?
-
+# keep track of good durations/offsets as a standard set
+    # pick specific note/scale/interval combinations
 
 
 # Define key and scale
@@ -25,21 +26,49 @@ scale = Scale(key, random.choice(scales))
 progression = Chord.progression(scale, base_octave=key.octave)
 chord = progression[0].invert_down()
 
+all_notes = []
+for chord in progression:
+    all_notes.append(chord.notes[0])
+    all_notes.append(chord.notes[1])
+    all_notes.append(chord.notes[2])
+pp.pprint(all_notes)
+
 time = 0.0 # Keep track of currect note placement time in seconds
 
 timeline = Timeline()
 
-offset = 1.5
-interval = 0.5
-notes = [0, 1, 2]
+notes = [0,1,2]
+random.shuffle(notes)
+random.shuffle(all_notes)
 
-pp.pprint(key)
-pp.pprint(scale)
-pp.pprint(notes)
+for i in range(25):
+    interval = 0.1
+    offset = 0.6
+    pp.pprint([key, scale, chord, progression, notes, interval])
 
-offset = 0.6
-interval = 0.2
-for i in range(80):
+    for j, n in enumerate(all_notes):
+        timeline.add(time + interval * j, Hit(n, 0.1))
+        #timeline.add(time + interval * j, Hit(chord.notes[n], 0.1))
+        
+        if(i < 20):
+            o_interval = interval + offset
+            timeline.add(time + o_interval * 1, Hit(n, 0.025))
+            #timeline.add(time + o_interval * 1, Hit(chord.notes[n], 0.05))
+    
+        time += 0.1
+
+        # TODO update this using noise/cos/sin
+        if(interval > 0.5):
+            interval -= 0.1
+        elif(interval < 0.0):
+            interval += 0.1
+        else:
+            interval = 0.1
+        
+
+    #pp.pprint([key, scale, chord, progression, notes, interval])
+    
+    """
     timeline.add(time + interval * 1, Hit(chord.notes[2].transpose(0), 8.0))
     timeline.add(time + interval * 2, Hit(chord.notes[1].transpose(0), 8.0))
     timeline.add(time + interval * 3, Hit(chord.notes[0].transpose(0), 8.0))
@@ -48,22 +77,32 @@ for i in range(80):
     timeline.add(time + interval * 6, Hit(chord.notes[0], 8.0))
 
     o_interval = interval + offset
-    timeline.add(time + o_interval * 1, Hit(chord.notes[0].transpose(0), 8.0))
+    timeline.add(time + o_interval * 1, Hit(chord.notes[2].transpose(0), 8.0))
     timeline.add(time + o_interval * 2, Hit(chord.notes[1].transpose(0), 8.0))
-    timeline.add(time + o_interval * 3, Hit(chord.notes[2].transpose(0), 8.0))
+    timeline.add(time + o_interval * 3, Hit(chord.notes[0].transpose(0), 8.0))
 
     time += 2.2
     interval += 0.025
+    """
+    if( i % 5 == 0 and i > 0):
+        chord = progression[ random.choice([0,1,2]) ]
 
     if(i % 10 == 0 and i > 0):
         scale = Scale(key, random.choice(scales))
         progression = Chord.progression(scale, base_octave=key.octave)
-        chord = progression[0]
+        chord = chord.invert_down()
+
+        all_notes = []
+        for chord in progression:
+            all_notes.append(chord.notes[0])
+            all_notes.append(chord.notes[1])
+            all_notes.append(chord.notes[2])
+        random.shuffle(all_notes)
 
     if(i % 20 == 0 and i > 0):
-        timeline.add(time + 0.03, Hit(chord.notes[0].transpose(0), 4.0))
+        timeline.add(time + 0.02, Hit(chord.notes[0].transpose(0), 4.0))
         timeline.add(time + 0.04, Hit(chord.notes[1].transpose(-6), 4.0))
-        timeline.add(time + 0.05, Hit(chord.notes[2].transpose(-12), 4.0))
+        timeline.add(time + 0.06, Hit(chord.notes[2].transpose(-12), 4.0))
         time += 4.0
 
 print("Rendering audio...")
