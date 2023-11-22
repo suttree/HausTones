87c36d087f37e8214e7a284198fecafdafb42749
Duncan Gough <duncan@suttree.com>
2023-11-22 17:12:20 +0000

    strummy

--- melodies/chords.py
+++ melodies/chords.py
@@ -23,7 +23,7 @@ time = 0.0 # Keep track of currect note placement time in seconds
 interval = random.uniform(0.8, 2.8)
 offset = 0.0
 iterations = random.randint(4, 10)
-duration = random.uniform(1.0, 8.0)
+duration = random.uniform(4.0, 12.0)
 
 timeline = Timeline()
 
@@ -36,6 +36,7 @@ for i in range(iterations):
         for note in notes:
             chord_progression.append(note)
 
+    spacer = random.uniform(0.01, 0.2)
     for index in chord_progression:
         offset = random.uniform(0.07, 2.625)
 
@@ -46,16 +47,36 @@ for i in range(iterations):
         #    chord = chord.augmented(chord.notes[0])
         #root, third, fifth = chord.notes
 
-        timeline.add(time + 0.00 + offset, Hit(chord.notes[0].transpose(-2), duration))
-        timeline.add(time + 0.00 + offset, Hit(chord.notes[2].transpose(-2), duration))
-        
-        timeline.add(time + 0.01 + offset, Hit(chord.notes[0], duration))
-        timeline.add(time + 0.02 + offset, Hit(chord.notes[2], duration))
-        timeline.add(time + 0.04 + offset, Hit(chord.notes[2], duration))
+        if(random.randint(0,1) > 0):
+            timeline.add(time + 0.00 + offset, Hit(chord.notes[0].transpose(-2), duration))
+            timeline.add(time + 0.00 + offset, Hit(chord.notes[2].transpose(-2), duration))
 
-        timeline.add(time + 0.06 + offset, Hit(chord.notes[0].transpose(12), duration))
-        timeline.add(time + 0.08 + offset, Hit(chord.notes[2].transpose(12), duration))
-        timeline.add(time + 0.10 + offset, Hit(chord.notes[2].transpose(12), duration))
+        if(random.randint(0,1) > 0):
+            timeline.add(time + (spacer * 1) + offset, Hit(chord.notes[0], duration))
+
+        timeline.add(time + (spacer * 2) + offset, Hit(chord.notes[2], duration))
+        
+        if(random.randint(0,1) > 0):
+            timeline.add(time + (spacer * 4) + offset, Hit(chord.notes[2], duration))
+
+        if(random.randint(0,6) > 5):
+            timeline.add(time + (spacer * 6) + offset, Hit(chord.notes[0].transpose(12), duration))
+        elif(random.randint(0,6) > 4):
+            timeline.add(time + (spacer * 6) + offset, Hit(chord.notes[0].transpose(12), duration))
+            timeline.add(time + (spacer * 8) + offset, Hit(chord.notes[2].transpose(12), duration))
+            timeline.add(time + (spacer * 10) + offset, Hit(chord.notes[2].transpose(12), duration))
+        elif(random.randint(0,6) > 3):
+            timeline.add(time + (spacer * 6) + offset, Hit(chord.notes[0].transpose(12), duration))
+            timeline.add(time + (spacer * 8) + offset, Hit(chord.notes[1].transpose(12), duration))
+            timeline.add(time + (spacer * 10) + offset, Hit(chord.notes[2].transpose(12), duration))
+        elif(random.randint(0,6) > 2):
+            timeline.add(time + (spacer * 6) + offset, Hit(chord.notes[2].transpose(12), duration))
+            timeline.add(time + (spacer * 8) + offset, Hit(chord.notes[1].transpose(12), duration))
+            timeline.add(time + (spacer * 10) + offset, Hit(chord.notes[2].transpose(12), duration))
+        elif(random.randint(0,1) > 0):
+            timeline.add(time + (spacer * 6) + offset, Hit(chord.notes[1].transpose(12), duration))
+            timeline.add(time + (spacer * 8) + offset, Hit(chord.notes[1].transpose(12), duration))
+            timeline.add(time + (spacer * 10) + offset, Hit(chord.notes[0].transpose(12), duration))
 
         time += interval * random.uniform(1.8, 2.2)
 
@@ -65,8 +86,8 @@ data = timeline.render()
 data = effect.tremolo(data, 0.1)
 #data = effect.flanger(data, 0.1)
 
-# Reduce volume to 25%
-data = data * 0.25
+# Reduce volume to 10%
+data = data * 0.10
 
 #print "Playing audio..."
 playback.play(data)
