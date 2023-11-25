b7a3a4b46e2c86eb9a5f7d128b4de9793aaad900
Duncan Gough <duncan@suttree.com>
2023-11-25 11:18:21 +0000

    add full path to せcript

--- pele.sh
+++ pele.sh
@@ -3,24 +3,27 @@
 # Directory containing the files
 DIR="melodies"
 
+# Get the full path of the directory
+FULL_DIR=$(realpath "$DIR")
+
 # Check if the directory exists
-if [ ! -d "$DIR" ]; then
-    echo "Directory $DIR does not exist."
+if [ ! -d "$FULL_DIR" ]; then
+    echo "Directory $FULL_DIR does not exist."
     exit 1
 fi
 
 # Get a list of .py files in the directory
-FILES=($DIR/*.py)
+FILES=($FULL_DIR/*.py)
 
 # Check if there are any .py files
 if [ ${#FILES[@]} -eq 0 ]; then
-    echo "No .py files found in $DIR."
+    echo "No .py files found in $FULL_DIR."
     exit 1
 fi
 
 # Pick a random file
 RANDOM_FILE=${FILES[RANDOM % ${#FILES[@]}]}
 
-# Execute the file
+# Execute the file with Python 3
 echo "Running: $RANDOM_FILE"
-"/usr/bin/env python3 $RANDOM_FILE"
+/usr/bin/env python3 "$RANDOM_FILE"
