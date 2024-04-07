7b701c365b9c5c395bc155897917141221b9f32a
Duncan Gough <duncan@suttree.com>
2024-04-07 22:28:50 +0100

    more vibes

--- melodies/oudh.py
+++ melodies/oudh.py
@@ -1,3 +1,5 @@
+# Je n'est vivre
+
 # To keep: oudh, pppony, Bb-asc/desc, slow
 # To run: venv/bin/python3.12 melodies/stepping.py
 
@@ -22,53 +24,33 @@ key_note = Note((random.choice(Note.NOTES), random.choice([0, 1, 2, 3, 4]))).not
 key = Note(key_note)
 
 #scales = ['chromatic']
-scales = ['major', 'pentatonicmajor', 'augmented', 'diminished', 'chromatic', 'wholehalf', 'halfwhole', 'wholetone', 'augmentedfifth', 'japanese', 'oriental', 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian']
-scale = Scale(key, random.choice(scales))
+scales = ['major', 'pentatonicmajor', 'japanese', 'diminished', 'locrian', 'ionian', 'mixolydian', 'phrygian']
+r_scale = random.choice(scales)
+scale = Scale(key, r_scale)
 notes = notes_from_scale(key.note, scale.intervals)
 notes_with_intervals = add_intervals_to_notes(notes)
 
 pp.pprint(key)
-pp.pprint(scale)
+pp.pprint(r_scale)
 pp.pprint(notes)
-pp.pprint(notes_with_intervals)
 
 # todo: use Ara melodies?
 # todo: more notes in notes_from_scale
 # todo: add interval to notes should pass a param that starts at 0.0 and increases (0.1, 0.2) etc, incl option to add noise to the increments)
+# can i switch scale after x number of iterations? with a arpeggio to indicate the end/beginning
 
-
-for i in range(3):
+for i in range(25):
     for j, note in enumerate(notes_with_intervals):
         interval = add_random_float(note[1], -1.25, 2.75)
         timeline.add(time + interval, Hit(Note(note[0]), duration))
+        pp.pprint([note[0], interval])
     time += duration
-
-
-#note = notes_with_intervals[0]
-#note2 = notes_with_intervals[1]
-#for i in range(3):
-#    timeline.add(time + note[1], Hit(Note(note[0]), duration))
-#    timeline.add(time + note2[1], Hit(Note(note2[0]), duration))
-#    time += duration
-
-#for i in range(2):
-#    for j, note in enumerate(notes_with_intervals):
-#        timeline.add(time + note[1], Hit(Note(note[0]), duration))
-#    time += duration
-
 
 print("Rendering audio...")
 data = timeline.render()
-#data = effect.shimmer(data, 2.047)
-data = effect.shimmer_wobble(data)
-#data = effect.tremolo(data, freq=1.1)
-#data = effect.pan(data, len(data) / 44100, 44100, pan_freq=0.2)
-#data = effect.feedback_modulated_delay(data, data, 1.26, 1.44)
-#data = effect.feedback_modulated_delay(data, data, 0.036, 0.64)
+data = effect.tremolo(data, freq=1.1)
 #data = effect.modulated_delay(data, data, 0.2, 1.8)
-#data = effect.flanger(data, 0.4)
-#data = effect.chorus(data, 0.45)
-#data = effect.reverb(data, 0.8, 0.025)
+data = effect.reverb(data, 0.8, 0.025)
 
 print("Playing audio...")
 playback.play(data)
