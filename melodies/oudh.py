105b1a21a1b345fa294c3a63fb27b1f479923cfb
Duncan Gough <duncan@suttree.com>
2024-04-08 22:14:00 +0100

    smoooooof

--- melodies/oudh.py
+++ melodies/oudh.py
@@ -25,32 +25,58 @@ key = Note(key_note)
 
 #scales = ['chromatic']
 scales = ['major', 'pentatonicmajor', 'japanese', 'diminished', 'locrian', 'ionian', 'mixolydian', 'phrygian']
-r_scale = random.choice(scales)
-scale = Scale(key, r_scale)
-notes = notes_from_scale(key.note, scale.intervals)
-notes_with_intervals = add_intervals_to_notes(notes)
 
-pp.pprint(key)
-pp.pprint(r_scale)
-pp.pprint(notes)
 
 # todo: use Ara melodies?
 # todo: more notes in notes_from_scale
 # todo: add interval to notes should pass a param that starts at 0.0 and increases (0.1, 0.2) etc, incl option to add noise to the increments)
-# can i switch scale after x number of iterations? with a arpeggio to indicate the end/beginning
 
-for i in range(25):
+r_scale = random.choice(scales)
+scale = Scale(key, r_scale)
+notes = notes_from_scale(key.note, scale.intervals)
+notes_with_intervals = add_intervals_to_notes(notes)
+pp.pprint(key)
+pp.pprint(r_scale)
+
+# Ascending arpeggio to open
+for j, note in enumerate(notes):
+    timeline.add(time + 0.1, Hit(Note(note), duration))
+time += duration
+    
+for i in range(75):
     for j, note in enumerate(notes_with_intervals):
         interval = add_random_float(note[1], -1.25, 2.75)
         timeline.add(time + interval, Hit(Note(note[0]), duration))
-        pp.pprint([note[0], interval])
     time += duration
+    
+r_scale = random.choice(scales)
+scale = Scale(key, r_scale)
+notes = notes_from_scale(key.note, scale.intervals)
+notes_with_intervals = add_intervals_to_notes(notes)
+pp.pprint(key)
+pp.pprint(r_scale)
+
+# And breathe....
+time += duration * 2
+
+for i in range(25):
+    for j, note in enumerate(notes_with_intervals):
+        interval = add_random_float(note[1], -0.25, 4.75)
+        timeline.add(time + interval, Hit(Note(note[0]), duration))
+    time += duration
+    
+# Descending arppegio to close
+for j, note in enumerate(notes[::-1]):
+    timeline.add(time + 0.1, Hit(Note(note), duration))
+time += duration
+
 
 print("Rendering audio...")
 data = timeline.render()
-data = effect.tremolo(data, freq=1.1)
-#data = effect.modulated_delay(data, data, 0.2, 1.8)
-data = effect.reverb(data, 0.8, 0.025)
+data = effect.tremolo(data, freq=1.47)
+data = effect.modulated_delay(data, data, 0.02, 0.03)
+data = effect.reverb(data, 0.8, 0.425)
+data = effect.flanger(data, 0.04)
 
 print("Playing audio...")
 playback.play(data)
