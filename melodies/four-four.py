c15125b8298fa32f2798fa4c16b4869417a7ee13
Duncan Gough <duncan@suttree.com>
2024-04-22 19:50:25 +0100

    new and updated

--- melodies/four-four.py
+++ melodies/four-four.py
@@ -22,7 +22,7 @@ interval = 2
 key_note = Note((random.choice(Note.NOTES), random.choice([1, 2, 3]))).note
 key = Note(key_note)
 
-scales = ['major', 'pentatonicmajor', 'japanese', 'diminished', 'locrian', 'ionian', 'mixolydian', 'phrygian']
+scales = ['major', 'pentatonicmajor', 'diminished', 'phrygian']
 
 r_scale = random.choice(scales)
 scale = Scale(key, r_scale)
@@ -31,22 +31,30 @@ notes_with_intervals = add_intervals_to_notes(notes)
 pp.pprint(key)
 pp.pprint(r_scale)
 
-for n in range(2):
-  for i in range(6):
-    for j, note in enumerate(notes_with_intervals[::-8]):
-      timeline.add(time + increment, Hit(Note(notes_with_intervals[0][0]), duration*2))
+for a in range(4):
+  #increment -= math.sin(increment)
+  #duration -= math.cos(increment)
+  for n in range(4):
+    #increment -= math.cos(increment)
+    #duration -= math.sin(increment)
+    for i in range(4):
+      #increment -= math.sin(increment)
+      #duration -= math.cos(increment)
+      for j, note in enumerate(notes_with_intervals[::-8]):
+        timeline.add(time + increment, Hit(Note(notes_with_intervals[0][0]), duration))
     
-    for j, note in enumerate(notes_with_intervals[::-1]):
-      timeline.add(time + math.sin(0.25) * j*i+1, Hit(Note(note[0]), duration))
+      for j, note in enumerate(notes_with_intervals[::-1]):
+        timeline.add(time + math.sin(0.25) * j*i+1, Hit(Note(note[0]), duration))
 
-    time += 2.0 + math.cos(time)
+      time += 2.0 + math.cos(time)
+      #duration -= math.cos(increment)
     
-    for j, note in enumerate(notes_with_intervals):
-        timeline.add(time + math.cos(j)*i*n, Hit(Note(note[0]), duration))
-    time += duration
+      for j, note in enumerate(notes_with_intervals):
+          timeline.add(time + math.cos(j)*i*n, Hit(Note(note[0]), duration))
 
-    for j, note in enumerate(notes_with_intervals[::8]):
-      timeline.add(time + increment, Hit(Note(notes_with_intervals[-1][0]), duration*2))
+      for j, note in enumerate(notes_with_intervals[::12]):
+        timeline.add(time + increment, Hit(Note(notes_with_intervals[-1][0]), duration*2))
+      time += duration
       
 print("Rendering audio...")
 data = timeline.render()
@@ -58,4 +66,4 @@ data = data * 0.1
 from musical.utils import save_normalized_audio
 save_normalized_audio(data, 44100, os.path.basename(__file__))
 
-#playback.play(data)
+playback.play(data)
