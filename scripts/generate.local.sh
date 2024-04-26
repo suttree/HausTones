a33b39394b4067d627489f2f5376ac31704ea1b8
Duncan Gough <duncan@suttree.com>
2024-04-26 19:29:03 +0100

    more tunes

--- scripts/generate.local.sh
+++ scripts/generate.local.sh
@@ -5,10 +5,10 @@
 #venv/bin/python3.12 melodies/hoot.py;
 #venv/bin/python3.12 melodies/repeatofuzzder.py;
 #venv/bin/python3.12 melodies/vibes.py;
+#venv/bin/python3.12 melodies/lullaby.py; 
+#venv/bin/python3.12 melodies/oudh.py;
 venv/bin/python3.12 melodies/tune.py;
 venv/bin/python3.12 melodies/droppedd.py;
-venv/bin/python3.12 melodies/lullaby.py; 
-venv/bin/python3.12 melodies/oudh.py;
 venv/bin/python3.12 melodies/vibes4u.py;
 venv/bin/python3.12 melodies/genevive.py;
 venv/bin/python3.12 melodies/beats.py;
