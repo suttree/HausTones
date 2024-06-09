aa37d9fdd79e2e049f550200a9d6684bc2aac288
Duncan Gough <duncan@suttree.com>
2024-06-09 15:09:01 +0100

    more tunes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,12 +4,12 @@ rm /home/pi/src/pele/output/*.wav
 python3 melodies/wed.py
 #python3 melodies/vibes4u.py
 python3 melodies/thu.py
-#python3 melodies/genevive.py
+python3 melodies/genevive.py
 #python3 melodies/hoo.py
 #python3 melodies/beats.py
 #python3 melodies/tune.py
-#python3 melodies/vibes2u.py
+python3 melodies/vibes2u.py
 #python3 melodies/vale.py
-#python3 melodies/holdingon.py
+python3 melodies/holdingon.py
 #python3 melodies/hoot.py
 #python3 melodies/magpie.py
