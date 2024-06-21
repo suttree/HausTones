42a523739c9911afc5d45f27ce93c43c767cc593
Duncan Gough <duncan@suttree.com>
2024-06-21 23:08:58 +0100

    updated melody, added to rotation

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,7 +4,7 @@ rm /home/pi/src/pele/output/*.wav
 python3 melodies/wed.py
 #python3 melodies/vibes4u.py
 python3 melodies/thu.py
-#python3 melodies/genevive.py
+python3 melodies/genevive.py
 #python3 melodies/hoo.py
 python3 melodies/beats.py
 #python3 melodies/tune.py
